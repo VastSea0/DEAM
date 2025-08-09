2d6757bd6f5b1429412af2244ca69a2735f902ac
VastSea0 <vastseaoffical0@outlook.com>
2025-08-09 23:56:20 +0300

    Refactor routing in App component and enhance index.css with custom styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './components/Home';
-import Privacy from './components/Privacy';
-import Versions from './components/Versions';
-import Pricing from './components/Pricing';
+import NotFound from './components/NotFound';
+
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/privacy" element={<Privacy />} />
-        <Route path="/versions" element={<Versions />} />
-        <Route path="/pricing" element={<Pricing />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
